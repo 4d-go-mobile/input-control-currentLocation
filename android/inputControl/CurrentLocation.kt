58b032b73c63c7b9b6adc364d6e16e824884aa08
Quentin Marciset <quentin.marciset@4d.com>
2022-10-17 16:28:39 +0200

    Fix issue with offline localisation

--- android/inputControl/CurrentLocation.kt
+++ android/inputControl/CurrentLocation.kt
@@ -1,14 +1,19 @@
 package ___PACKAGE___
 
 import android.location.Location
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.qmobile.qmobiledatasync.utils.BaseKotlinInputControl
 import com.qmobile.qmobiledatasync.utils.KotlinInputControl
-import com.qmobile.qmobileui.ui.SnackbarHelper
 import com.qmobile.qmobileui.utils.PermissionChecker
+import java.util.concurrent.TimeUnit
 
 @KotlinInputControl
 class CurrentLocation(private val view: View) : BaseKotlinInputControl {
@@ -17,24 +22,44 @@ class CurrentLocation(private val view: View) : BaseKotlinInputControl {
 
     private val rationaleString = "Permission required to access current location"
 
+    private lateinit var outputCallback: (outputText: String) -> Unit
+
     private var fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(view.context as FragmentActivity)
 
-    private lateinit var outputCallback: (outputText: String) -> Unit
+    private lateinit var locationCallback: LocationCallback
 
     @Suppress("MissingPermission")
     private fun getLocation() {
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-            if (location != null) {
-                val latLon = "+" +
-                    location.latitude.toString().removePrefix("+") +
-                    "-" +
-                    location.longitude.toString().removePrefix("-")
-                outputCallback(latLon)
-            } else {
-                SnackbarHelper.show(view.context as FragmentActivity, "Could not get current location")
-                outputCallback("")
+        val locationRequest = LocationRequest.create().apply {
+            interval = TimeUnit.SECONDS.toMillis(2)
+            fastestInterval = TimeUnit.SECONDS.toMillis(3)
+            maxWaitTime = TimeUnit.SECONDS.toMillis(3)
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult.lastLocation?.let {
+                    proceedLocationResult(it, false)
+                }
             }
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        fusedLocationClient.lastLocation.addOnSuccessListener { lastKnownLocation: Location? ->
+            if (lastKnownLocation != null) {
+                proceedLocationResult(lastKnownLocation, true)
+            }
+        }
+    }
+
+    private fun proceedLocationResult(location: Location, isLastKnownLocation: Boolean) {
+        val latLon = "${location.latitude}, ${location.longitude}"
+        outputCallback(latLon)
+        if (!isLastKnownLocation) {
+            fusedLocationClient.removeLocationUpdates(locationCallback)
         }
     }
 
@@ -48,7 +73,7 @@ class CurrentLocation(private val view: View) : BaseKotlinInputControl {
     }
 
     private fun requestPermission(permission: String, canGoOn: () -> Unit) {
-        (view.context as PermissionChecker?)?.askPermission(
+        (view.context as? PermissionChecker)?.askPermission(
             permission = permission,
             rationale = rationaleString
         ) { isGranted ->
